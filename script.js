76541e5c2e7c3984310f0bd41301812cf01f4fe4
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-03-11 04:35:39 +0530

    added songs and posters

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 
 
-
-async function main(){
-
-    let a = await fetch("http://192.168.1.33:60816/Downloads/songs/")
-    let responses = await a.text()
-    console.log(responses)
+ async function getSongs(){
+    let a = await fetch("./songs")
+    let response = await a.text();
+    // console.log(response)
     let div = document.createElement("div")
-    div.innerHTML = responses;
-    let tds = div.getElementsByTagName("td")
-    console.log(tds)
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for(let index = 0; index < as.length; index++){
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+    return songs
+ }
+
+ async function main(){
+ let songs = await getSongs()
+ console.log(songs)
+
+
+ let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+   for(const song of songs){
+    let Song = song.replaceAll("http://127.0.0.1:3001/songs/"," " )
+
+
+    songUL.innerHTML = songUL.innerHTML + `<li>${Song.replaceAll("%20"," ")}</li>`;
+ }
+
+    var audio = new Audio(songs[0]);
+    
+
+    document.getElementById("Play").addEventListener("click",()=>{
+        audio.play().catch((error)=>{
+            console.error("Error ",error)
+        })
+
+        audio.addEventListener("ontimeupdate", () => {
+            console.log(audio.duration,audio.currentSrc,audio.currentTime)
+            // The duration variable now holds the duration (in seconds) of the audio clip
+          });
+    })
 }
 
 main()
